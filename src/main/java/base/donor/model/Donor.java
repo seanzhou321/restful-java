731b40debb0f3e18fd5c1270bc5d32ff73c0c493
y0z04y3 <Yixiong.Zhou@walmart.com>
2021-08-13 20:25:53 -0700

    update donor

--- src/main/java/base/donor/model/Donor.java
+++ src/main/java/base/donor/model/Donor.java
@@ -4,12 +4,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 @Entity
+@Table(name = "DONOR")
 public class Donor implements Cloneable {
 
   public Donor() {}
---
+++ src/test/java/base/donor/controller/Demo.java
@@ -0,0 +1,9 @@
+package base.donor.controller;
+
+public class Demo {
+  public static void main(String[] args) {
+    int a = -425798752;
+    System.out.println(Character.MIN_RADIX + " "+ Character.MAX_RADIX+" "+Integer.toUnsignedString(a, 36));
+    System.out.println(Character.MIN_RADIX + " "+ Character.MAX_RADIX+" "+Integer.toUnsignedString(a, 6));
+  }
+}
